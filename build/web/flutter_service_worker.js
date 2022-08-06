409e9af80df0abb724a845cb4c32a60d6b0a783d
Usama0444 <rajausama0444@gmail.com>
2022-08-06 17:31:26 +0500

    release

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d3f66c083ae38020c1c402e0098bf4cf",
-"/": "d3f66c083ae38020c1c402e0098bf4cf",
-"main.dart.js": "493ca8bacfe616aaca2478069270da99",
+"index.html": "dd5501ec3c4170409f7010ae2f499f65",
+"/": "dd5501ec3c4170409f7010ae2f499f65",
+"main.dart.js": "4824f736d7b6d762cffcf9af0ab044b6",
 "manifest.json": "be04301c1cfb3983f4854a830ee8c9ce",
 "version.json": "f1437db373b8b8eaeddf8739129e17e9"
 };
