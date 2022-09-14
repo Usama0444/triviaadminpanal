f0542a5557baa40fff810c82a7a7976b54885f86
Usama0444 <ughafoor482@gmail.com>
2022-09-14 17:01:23 +0500

    push

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Downloads\\flutter_windows_3.0.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\FlutterWIithSoftwareHub\\triviaadminpanal" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Downloads\\flutter_windows_3.0.5-stable\\flutter"
+  "PROJECT_DIR=F:\\FlutterWIithSoftwareHub\\triviaadminpanal"
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Downloads\\flutter_windows_3.0.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\FlutterWIithSoftwareHub\\triviaadminpanal\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\FlutterWIithSoftwareHub\\triviaadminpanal"
+  "FLUTTER_TARGET=F:\\FlutterWIithSoftwareHub\\triviaadminpanal\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\FlutterWIithSoftwareHub\\triviaadminpanal\\.dart_tool\\package_config.json"
+)
